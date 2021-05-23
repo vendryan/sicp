00d4fdddb073f739d86d363869ca84bc198a3ce1
vendryan <49556626+vendryan@users.noreply.github.com>
2021-05-23 14:10:20 +0700

    Create sicp2-16 no ans.scm

---
+++ Chapter 2/sicp2-16 no ans.scm
@@ -0,0 +1,10 @@
+; Because at each computation there is an error
+; And that equivalent equation compute differently
+; and hence it's different
+
+; maybe we need to make a fraction package
+; represent interval as fraction that have numerator and
+; denominator and when we need it divide
+; numer with denom. This way it's accurate and
+; Representing integer is easier for computer than 
+; representing floating point
