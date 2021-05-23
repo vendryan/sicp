0129b1654c1ec2e5368b08f10b54c2f8faaf5855
vendryan <49556626+vendryan@users.noreply.github.com>
2021-05-23 13:10:43 +0700

    Create sicp2-12.scm

---
+++ Chapter 2/sicp2-12.scm
@@ -0,0 +1,79 @@
+; Print
+(define (print line)
+    (display line)
+    (newline))
+; Check if given number is negative or not
+(define (negative? x)
+    (< x 0))
+; Make interval
+(define (make-interval a b)
+    (if (< b a) 
+        (cons b a)
+        (cons a b)))
+; Way to get upper bound and lower bound of
+; given interval
+(define (lower-bound interval)
+    (car interval))
+(define (upper-bound interval)
+    (cdr interval))
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (print-interval interval)
+    (display (lower-bound interval))
+    (display " < x < ")
+    (display (upper-bound interval))
+    (newline))
+
+(define (add-interval x y)
+    (make-interval (+ (lower-bound x) (lower-bound y))
+                   (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+    (let ((p1 (* (lower-bound x) (lower-bound y)))
+          (p2 (* (lower-bound x) (upper-bound y)))
+          (p3 (* (upper-bound x) (lower-bound y)))
+          (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+    (if (or (= (upper-bound y) 0) (= (lower-bound y) 0))
+        (error "Division by zero error") ; if divisor is zero raise error
+        (mul-interval x 
+                      (make-interval (/ 1.0 (upper-bound y))
+                                     (/ 1.0 (lower-bound y))))))
+
+; difference of interval is just upper bound - lower bound
+; procedure that get the difference of given interval
+(define (sub-interval interval)
+    (- (upper-bound interval)
+       (lower-bound interval)))
+
+; *** Answer here ***
+(define (make-center-percent center percentage)
+    (let ((deviation (* (/ percentage 100.0) 
+                        (abs center))))
+        (make-interval (- center deviation)
+                       (+ center deviation))))
+
+(define (percent interval)
+    (let ((ctr (center interval))
+          (lx (lower-bound interval)))
+      (let ((deviation (- ctr lx)))
+          (abs (* (/ deviation ctr) 100)))))
+
+; Test
+(define test (make-center-percent -300 20))
+(define test2 (make-center-percent 35 10))
+(define test3 (make-center-percent 50 0))
+(print-interval test)
+(print (percent test)) ; Should be 20
+(print-interval test2)
+(print (percent test2)) ; Should be 10
+(print-interval test3)
+(print (percent test3)) ; Should be 0
