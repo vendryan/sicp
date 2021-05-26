3c0c72aa6113f42b32fb2f971fdc1510e93c3c08
vendryan <49556626+vendryan@users.noreply.github.com>
2021-05-26 15:38:20 +0700

    Update sicp2-29.scm

--- Chapter 2/sicp2-29.scm
+++ Chapter 2/sicp2-29.scm
@@ -14,6 +14,11 @@
     (car branch))
 (define (branch-structure branch)
     (cadr branch))
+(define (total-weight mobile)
+    (cond ((null? mobile) 0)
+          ((not (pair? mobile)) mobile) ; means its a number
+          (else (+ (total-weight (branch-structure (left-branch mobile)))
+                   (total-weight (branch-structure (right-branch mobile)))))))
 
 (define branch1 (make-branch 5 10))
 (define branch2 (make-branch 3 4))
@@ -22,6 +27,5 @@
 (define branch4 (make-branch 3 mobile1))
 (define mobile2 (make-mobile branch3 branch4))
 
-
-
 (print mobile2)
+(print (total-weight mobile2)) ; expected 21 and it's really produce 21
