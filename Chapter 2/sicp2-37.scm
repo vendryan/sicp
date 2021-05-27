9739d45caa360ec73874ac5b6d0bddf21fdc60af
vendryan <49556626+vendryan@users.noreply.github.com>
2021-05-27 21:27:33 +0700

    Create sicp2-37.scm

---
+++ Chapter 2/sicp2-37.scm
@@ -0,0 +1,26 @@
+(define nil '())
+(define (print line)
+    (display line)
+    (newline))
+
+(define (accumulate op initial sequence)
+    (if (null? sequence)
+        initial
+        (op (car sequence)
+            (accumulate op initial (cdr sequence)))))
+(define (accumulate-n op init seqs)
+    (if (null? (car seqs))
+        nil
+        (cons (accumulate op init (map car seqs))
+              (accumulate-n op init (map cdr seqs)))))
+
+(define (dot-product v w)
+    (accumulate + 0 (map * v w)))
+(define (matrix-*-vector m v)
+    (map (lambda (x) (dot-product x v))
+         m))
+
+
+(print (dot-product (list 4 5 6) (list 1 2 3)))
+(print (matrix-*-vector (list (list 1 2 3) (list 4 5 6))
+                        (list 1 2 3)))
