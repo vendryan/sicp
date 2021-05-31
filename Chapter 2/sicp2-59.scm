87e58303ab6125e9d67366fd094215d2c7b0504d
vendryan <49556626+vendryan@users.noreply.github.com>
2021-05-31 18:13:41 +0700

    Create sicp2-59.scm

---
+++ Chapter 2/sicp2-59.scm
@@ -0,0 +1,32 @@
+(define (print line)
+    (display line)
+    (newline))
+
+; Set representation as unordered list
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? (car set) x) #t)
+        (else (element-of-set? x (cdr set)))))
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2))
+          '())
+        ((element-of-set? (car set1) set2)
+          (cons (car set1) (intersection-set (cdr set1)
+                                             set2)))
+        (else (intersection-set (cdr set1) set2))))
+(define (union-set set1 set2)
+  (cond ((or (null? set1) (null? set2))
+          set2)
+        ((element-of-set? (car set1) set2)
+          (union-set (cdr set1) set2))
+        (else 
+          (cons (car set1) (union-set (cdr set1) set2)))))
+
+(define test (list 3 4 5 6 7 8))
+(define test2 (list 1 2 3))
+(print (intersection-set test test2))
+(print (union-set test test2))
