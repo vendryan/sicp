84e0fe7a04c9c2e67feb3d61db1c683e7ec3c8de
vendryan <49556626+vendryan@users.noreply.github.com>
2021-06-05 19:23:41 +0700

    Update sicp3-1.scm

--- Chapter 3/sicp3-1.scm
+++ Chapter 3/sicp3-1.scm
@@ -1,1 +1,22 @@
-(display "hello world")
+(define (print line)
+  (display line)
+  (newline))
+
+(define balance 100)
+
+(define (withdraw amount)
+  (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds"))
+
+(define new-withdraw
+  (let ((balance 100))
+       (lambda (amount)
+         (if (>= balance amount)
+             (begin (set! balance (- balance amount))
+                    balance)
+             "Insufficient funds"))))
+
+(print (new-withdraw 100))
+(print (new-withdraw 50))
