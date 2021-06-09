5ec5ae476d27ea1ee26ef7791217cb7d15ff21f4
vendryan <49556626+vendryan@users.noreply.github.com>
2021-06-09 19:41:21 +0700

    Update sicp3-23.scm

--- Chapter 3/sicp3-23.scm
+++ Chapter 3/sicp3-23.scm
@@ -21,6 +21,7 @@
 ; CONSTRUCTOR
 (define (make-deque) (cons '() '()))
 
+; MUTATOR
 (define (front-deque-insert! deque item)
   (let ((pointer-pair (cons '() '())))
     (let ((new-pair (cons item '())))
@@ -29,4 +30,17 @@
               (set-front-ptr! deque pointer-pair)
               (set-rear-ptr! deque pointer-pair))
             (else
-              
+              (set-cdr! (deque-item-pair deque) pointer-pair)
+              (set-cdr! pointer-pair (front-ptr deque))
+              (set-car! pointer-pair new-pair)
+              (set-front-ptr! deque pointer-pair))))))
+
+
+
+(define d1 (make-deque))
+(front-deque-insert! d1 'a)
+(print d1)
+(front-deque-insert! d1 'b)
+(print d1)
+(print (front-deque d1))
+(print (rear-deque d1))
