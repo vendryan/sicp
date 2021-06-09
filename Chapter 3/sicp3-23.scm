10c5a9a050a556c8c6497e7accb78a0d70235001
vendryan <49556626+vendryan@users.noreply.github.com>
2021-06-09 19:30:09 +0700

    Update sicp3-23.scm

--- Chapter 3/sicp3-23.scm
+++ Chapter 3/sicp3-23.scm
@@ -3,13 +3,30 @@
   (newline))
 
 ; SELECTOR
-(define (front-ptr queue) (car queue))
-(define (rear-ptr queue) (cdr queue))
-(define (set-front-ptr! queue item) 
-  (set-car! queue item))
-(define (set-rear-ptr! queue item) 
-  (set-cdr! queue item))
-(define (empty-queue? queue)
-  (null? (front-ptr queue)))
-(define (front-queue queue)
-  (caar (front-ptr queue)))
+(define (front-ptr deque) (car deque))
+(define (rear-ptr deque) (cdr deque))
+(define (set-front-ptr! deque item) 
+  (set-car! deque item))
+(define (set-rear-ptr! deque item) 
+  (set-cdr! deque item))
+(define (empty-deque? deque)
+  (null? (front-ptr deque)))
+(define (front-deque deque)
+  (caar (front-ptr deque)))
+(define (rear-deque deque)
+  (caar (rear-ptr deque)))
+(define (deque-item-pair deque)
+  (car (front-ptr deque)))
+
+; CONSTRUCTOR
+(define (make-deque) (cons '() '()))
+
+(define (front-deque-insert! deque item)
+  (let ((pointer-pair (cons '() '())))
+    (let ((new-pair (cons item '())))
+      (cond ((empty-deque? deque)
+              (set-car! pointer-pair new-pair)
+              (set-front-ptr! deque pointer-pair)
+              (set-rear-ptr! deque pointer-pair))
+            (else
+              
