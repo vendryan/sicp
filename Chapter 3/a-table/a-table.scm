5211ff796f359356b766dad342814ad77a400405
vendryan <49556626+vendryan@users.noreply.github.com>
2021-06-10 16:57:23 +0700

    Create a-table.scm

---
+++ Chapter 3/a-table/a-table.scm
@@ -0,0 +1,21 @@
+(define (make-table)
+  (cons '*table* '()))
+
+(define (assoc key table)
+  (if ((null? table) #f)
+      ((equal? key (caar records)) (car records))
+      (else (assoc key (cdr records)))))
+
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (cdr record)
+        false)))
+  
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (set-cdr! record value)
+        (set-cdr! table
+                  (cons (cons key value) (cdr table)))))
+  'ok)
