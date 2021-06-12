9ca32c3a436be820e72b5520c029328a792b72ad
vendryan <49556626+vendryan@users.noreply.github.com>
2021-06-11 20:13:17 -0700

    Update a-circuit-simulator.scm

--- Chapter 3/a-circuit-simulator.scm
+++ Chapter 3/a-circuit-simulator.scm
@@ -69,6 +69,23 @@
             (else (error "Unknown operation -- WIRE" m))))
     dispatch))
 
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
 (define the-agenda (make-agenda))
 (define inverter-delay 2)
 (define and-gate-delay 3)
