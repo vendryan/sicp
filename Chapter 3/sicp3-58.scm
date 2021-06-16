0cada3d6a2327e8983124c25125d524fcd51a216
vendryan <49556626+vendryan@users.noreply.github.com>
2021-06-16 20:48:22 +0700

    Create sicp3-58.scm

---
+++ Chapter 3/sicp3-58.scm
@@ -0,0 +1,21 @@
+(define (expand num den radix)
+  (cons-stream
+   (quotient (* num radix) den)
+   (expand (remainder (* num radix) den) den radix)))
+
+(expand 1 7 10)
+; The evolution of (expand 1 7 10)
+; (cons-stream 1 (expand 3 7 10))
+; (cons-stream 1
+;              (cons-stream 4 (expand 2 7 10)))
+; (cons-stream 1
+;              (cons-stream 4
+;                           (cons-stream 2
+;                                        (expand 6 7 10))))
+; (cons-stream 1
+;              (cons-stream 4
+;                           (cons-stream 2
+;                                        (cons-stream 8
+;                                                     (expand 4 7 10)))))
+; 1, 4, 2, 8, 5 (expand 5 7 10)
+; 1, 4, 2, 8, 5, 7 (expand 1 7 10) (alternate again)
