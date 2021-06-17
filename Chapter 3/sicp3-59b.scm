d0e30b870b6cd277c4d735bd9a530cfa4d9cc785
vendryan <49556626+vendryan@users.noreply.github.com>
2021-06-17 14:22:52 +0700

    Create sicp3-59b.scm

---
+++ Chapter 3/sicp3-59b.scm
@@ -0,0 +1,29 @@
+(define (integers-starting-from n)
+  (cons-stream n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+(define (divisible? x y) (= (remainder x y) 0))
+
+(define (integrate-series s)
+; elegant approach: (stream-map / s integers))
+;  (define (integrate s n)
+;    (cons-stream (stream-car s)
+;                 (stream-map (lambda (x) (* x (/ 1 n)))
+;                             (integrate s (+ n 1)))))
+  (define (integrate s n)
+    (cons-stream (/ (stream-car s) n)
+                 (integrate (stream-cdr s) (+ n 1))))
+  (integrate s 1))
+
+(define (partial-sum s)
+  (cons-stream (stream-car s)
+               (stream-map (stream-cdr s)
+                           (partial-sum s))))
+
+(define sine-series
+  (cons-stream 0 (integrate-series cosine-series)))
+(define cosine-series
+  (cons-stream 1 (integrate-series (stream-map - sine-series))))
+
+(display-top10 (partial-sum sine-series))
