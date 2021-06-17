8a875ce450eedfdf73ff8d4c53206d0bfb2a19d3
vendryan <49556626+vendryan@users.noreply.github.com>
2021-06-17 15:42:59 +0700

    Create sicp3-64.scm

---
+++ Chapter 3/sicp3-64.scm
@@ -0,0 +1,23 @@
+(define (average x y)
+  (/ (+ x y) 2))
+(define (sqrt-improve guess x)
+  (average guess (/ x guess)))
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream 1.0
+                 (stream-map (lambda (guess)
+                               (sqrt-improve guess x))
+                             guesses)))
+  guesses)
+
+(define (stream-limit s tolerance)
+  (let ((s0 (stream-ref s 0))
+        (s1 (stream-ref s 1)))
+    (if (< (abs (- s0 s1)) tolerance)
+        s1
+        (stream-limit (stream-cdr s) tolerance))))
+
+(define (sqrt x tolerance)
+  (stream-limit (sqrt-stream x) tolerance))
+
+(print (sqrt 2 0.001))
