4b4fd40495131d963507424adda7119f721832c6
vendryan <49556626+vendryan@users.noreply.github.com>
2021-06-18 17:17:01 +0700

    Update sicp3-66.scm

--- Chapter 3/sicp3-66.scm
+++ Chapter 3/sicp3-66.scm
@@ -1,3 +1,17 @@
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+       (stream-map (lambda (x) (list (stream-car s) x))
+                   (stream-cdr t))
+       (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
 ; (1 1) <-- 1
 ; (1 2) 1
 ; (2 2) <-- 3
@@ -30,6 +44,29 @@
 ; (1 16)
 ; (5 5) <-- 31
 
-; We can see that the pair (x y) where x = y
+; Let's call same-pair as (x y) where x = y
+; We can see that same-pair
 ; Appear every 1 + 2 + 4 + ... 2^n where
-; n start from 0
+; n start from 0 and inbetween them appear the
+; number that is the same-pair in 1 + 2 + 4 + ... + 2^n times
+; Starting from that part
+; lets call Sn = 1 + 2 + 4 + ... + 2^n
+
+; using this we can know that (1 100) appears in the
+; part where Sn + 1 > 100 (+ 1 from the same-pair)
+
+; (99 100) will appear somewhere around
+; S98 and S99 which is very big and far
+; as for (100 100) will appear exactly at S99
+
+(define (expt-series b n)
+  (cons-stream (expt b n)
+               (expt-series b (+ n 1))))
+
+(define Sn (partial-sum (expt-series 2 0)))
+(define mystery (pairs integers integers))
+
+; minus 1 because stream-ref counting start from zero
+; we can't compute S99 because it get really big
+(print (stream-ref mystery (- (stream-ref Sn 15) 1)))
+(print (- (stream-ref Sn 15) 1))
