a6068cca9b7cc198ba976c04a5015b1dc5636c91
vendryan <49556626+vendryan@users.noreply.github.com>
2021-06-19 17:28:12 +0700

    Create sicp3-72.scm

---
+++ Chapter 3/sicp3-72.scm
@@ -0,0 +1,77 @@
+(define (merge weight-proc s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+          (let ((x1 (stream-car s1))
+                (x2 (stream-car s2)))
+            (cond ((> (weight-proc x1) (weight-proc x2))
+                    (cons-stream x2
+                                 (merge weight-proc
+                                        s1
+                                        (stream-cdr s2))))
+                  ((> (weight-proc x1) (weight-proc x2))
+                    (cons-stream x1
+                                 (merge weight-proc
+                                        (stream-cdr s1)
+                                        s2)))
+                  (else (cons-stream x1
+                                 (merge weight-proc
+                                        (stream-cdr s1)
+                                        s2))))))))
+                                     
+(define (pairs s t weight)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (merge
+    weight
+    (stream-map (lambda (x) (list (stream-car s) x))
+                (stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t) weight))))
+
+; Tweaked this so display what pair that fulfill
+; not the value result
+(define (consecutive-value s weight times)
+  (define (iter s val how-many accumulated)
+    (cond ((stream-null? s) the-empty-stream)
+          ((null? val) (iter (stream-cdr s) 
+                             (weight (stream-car s))
+                             (+ how-many 1)
+                             (cons (stream-car s) accumulated)))
+          ((= how-many times)
+            (cons-stream accumulated
+                         (iter (stream-cdr s)
+                               val
+                               0
+                               '())))
+          ((not (= val (weight (stream-car s))))
+            (iter (stream-cdr s)
+                  (weight (stream-car s))
+                  1
+                  (cons (stream-car s) '())))
+          (else (iter (stream-cdr s)
+                      val
+                      (+ how-many 1)
+                      (cons (stream-car s) accumulated)))))
+  (iter s '() 0 '()))
+
+(define (cube x)
+  (* x x x))
+
+(define (weight-proc x)
+  (+ (cube (car x))
+     (cube (cadr x))))
+
+(define (sum-square x)
+  (+ (square (car x))
+     (square (cadr x))))
+  
+(define weighted-cube-pair (pairs integers integers weight-proc))
+(define ramanujan-number (consecutive-value weighted-cube-pair
+                                            weight-proc
+                                            2))
+(define three-consecutive-sum-square
+  (consecutive-value (pairs integers integers sum-square)
+                     sum-square
+                     3))
+  
+(display-top10 three-consecutive-sum-square)
